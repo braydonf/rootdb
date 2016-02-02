8b62d18bb64f4df5aee158da6c6391f7136df1b8
Braydon Fuller <courier@braydon.com>
2016-02-01 21:04:21 -0500

    Tests: Create directory if it doesn't exist

--- src/rootdb.c
+++ src/rootdb.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <openssl/sha.h>
 #include <lmdb.h>
 
 int rootdb_open(MDB_env* mdb_env, const char* path)
 {
+  struct stat st = {0};
+
+  if (stat(path, &st) == -1) {
+    mkdir(path, 0755);
+  }
+
   int mdb_flags = MDB_INTEGERKEY | MDB_CREATE;
   int status;
   status = mdb_env_create(&mdb_env);
